2532f0713454ed93a581af452a5261572d6df2c9
AKshattrived <akshattrivedi7901@gmail.com>
2022-12-03 18:30:15 +0530

    Dynamic looping with jsx

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,20 @@ import React, { Component } from "react";
 
 export default class NewsItem extends Component {
   render() {
-    let { title, description } = this.props;
+    let { title, description, imgUrl, newsUrl } = this.props;
     return (
-      <div>
+      <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img
-            src="https://a57.foxnews.com/cf-images.us-east-1.prod.boltdns.net/v1/static/694940094001/0b979717-f563-4dd3-ae3f-2ff3e1ef47c5/7130512a-03f4-47b6-b588-1cea1e1d3e18/1280x720/match/1024/512/image.jpg?ve=1&tl=1"
-            className="card-img-top"
-            alt="..."
-          />
+          <img src={imgUrl} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/" className="btn btn-primary">
-              Go somewhere
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              Read More
             </a>
           </div>
         </div>
